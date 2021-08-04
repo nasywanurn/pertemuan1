d58e8c5e59300fbb202224d26842f0e81fa58d48
nasywa554 <nasywanurnafilah554@gmail.com>
2021-08-04 15:37:51 +0700

    pembelajaran

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Kelompok 5",
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
+});
+
